5771e0bc4d047164fee09c15821ed59eaa89116e
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-09-19 10:21:07 -0300

    Exercício 2 concluído

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	public class DadosFuncionario
+	{
+		public static string Nome;
+		public static double SalarioBruto;
+		public static double Imposto;
+		public static double Porcentagem;
+
+		public static double SalarioLiquido()
+		{
+			return SalarioBruto - Imposto;
+		}
+		public static double AumentarSalario()
+		{
+			return (SalarioBruto - Imposto) + (SalarioBruto * (Porcentagem / 100));
+		}
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            DadosFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            DadosFuncionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            DadosFuncionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario1 = DadosFuncionario.SalarioLiquido();
+            Console.WriteLine("Funcionário: " + DadosFuncionario.Nome + ", R$" + salario1.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            DadosFuncionario.Porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario2 = DadosFuncionario.AumentarSalario();
+            Console.WriteLine("Dados atualizados: " + DadosFuncionario.Nome + ", R$" + salario2.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
